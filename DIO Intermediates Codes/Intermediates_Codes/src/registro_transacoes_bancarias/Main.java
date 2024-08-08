215f5d050eab365ae05a73168577fb9f672ff047
AndreFeh <andrefelipe.c.santos@gmail.com>
2024-08-08 15:16:45 -0300

    adicionando java code intermediarios

---
+++ DIO Intermediates Codes/Intermediates_Codes/src/registro_transacoes_bancarias/Main.java
@@ -0,0 +1,15 @@
+package registro_transacoes_bancarias;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Double> transacoes = new ArrayList<>();
+
+
+
+    }
+}
